15d96cb5f491d8bfd4d4cf0120f25d97b28bc43f
sannepetersvirtualgraphicdesigner <60850206+sannepetersvirtualgraphicdesigner@users.noreply.github.com>
2021-01-31 13:52:41 +0100

    Target search value searchengine

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
+  const [city, setCity] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert("Searching");
+    alert(`Searching ${city}`);
+  }
+  function handleCityChange(event) {
+    event.preventDefault();
+    setCity(event.target.value);
   }
   return (
     <div className="col-4">
@@ -16,6 +22,7 @@ export default function SearchEngine() {
             className="form-control"
             autoComplete="off"
             spellCheck="false"
+            onChange={handleCityChange}
           />
           <button className="button">
             <i className="fas fa-search search-icon"></i>
